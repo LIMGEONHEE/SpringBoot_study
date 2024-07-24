84a74f61a0ba407d71762cde2bea8e64438ac4cd
LIMGEONHEE <dlarjsgml321@naver.com>
2024-07-24 16:55:47 +0900

    7/24

--- springboot3/src/main/java/com/itwill/springboot3/repository/EmployeeRepository.java
+++ springboot3/src/main/java/com/itwill/springboot3/repository/EmployeeRepository.java
@@ -3,12 +3,17 @@ package com.itwill.springboot3.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.itwill.springboot3.domain.Employee;
+import java.time.LocalDate;
+
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
     // JPA query method 작성 방법:
-    // JPA에서 약속된 키워드들을 메서드 이름을 작성할 때 사용.
+    // JPA에서 약속된 키워드들과 엔터티의 필드 이름들을 사용해서
+    // 메서드 이름을 (camel 표기법으로) 작성.
 
     // select * from employees where department_id = ?
     List<Employee> findByDepartmentId(Integer id);
@@ -32,4 +37,63 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
     // 이름에 포함된 단어로 검색, 단어 대/소문자 구분 없이 검색, 정렬 순서는 이름 내림차순.
     // select * from employees where upper(first_name) like upper(?) order by first_name desc
     List<Employee> findByFirstNameContainingIgnoreCaseOrderByFirstNameDesc(String keyword);
+
+    // 급여가 어떤 값을 초과하는 직원들의 정보
+    // select * from employees where salary > ?
+    List<Employee> findBySalaryGreaterThan(Integer salary);
+
+    // 급여가 어떤 값 미만인 직원들의 정보(where salary < ?)
+    List<Employee> findBySalaryLessThan(Integer salary);
+
+    // 급여가 어떤 범위 안에 있는 직원들의 정보(where salary between ?1 and ?2)
+    List<Employee> findBySalaryBetween(Integer startSalary, Integer endSalary);
+
+    // 입사날짜가 특정 날짜 이전인 직원들의 정보(where hire_date < ?)
+    List<Employee> findByHireDateBefore(LocalDate hireDate);
+
+    // 입사날짜가 특정 날짜 이후인 직원들의 정보(where hire_date > ?)
+    List<Employee> findByHireDateAfter(LocalDate hireDate);
+
+    // 입사날짜가 날짜 범위 안에 있는 직원들의 정보(where hire_date between ?1 and ?2)
+    List<Employee> findByHireDateBetween(LocalDate startHireDate, LocalDate endHireDate);
+
+    // 부서 이름으로 직원 검색
+    List<Employee> findByDepartmentDepartmentName(String name);
+
+    // 근무 도시 이름으로 직원 검색
+    List<Employee> findByDepartmentLocationCity(String city);
+
+    // 대소문자 구분없이 성(lastName)과 이름(firstName)에 문자열이 포함된 직원 찾기:
+    List<Employee> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(String firstName, String lastName);
+
+    // JPQL(Java Persistence Query Language)
+    // JPA에서 사용하는 "객체지향(object-oriented)" 쿼리 문법.
+    // 테이블 이름과 테이블 컬럼 이름으로 쿼리 문장을 작성하는 것이 아니라,
+    // 엔터티 객체 이름과 엔터티 필드 이름으로 쿼리를 작성하는 문법.
+    // alias(별명)을 반드시 사용해야 함.
+    // 엔터티 이름과 필드 이름은 대소문자를 구분.
+
+    @Query("select e from Employee e "
+            + "where upper(e.firstName) like upper('%' || ?1 || '%') "
+            + "or upper(e.lastName) like upper('%' || ?2 || '%')")
+    List<Employee> findByName(String firstName, String lastName);
+
+    @Query("select e from Employee e "
+            + "where upper(e.firstName) like upper('%' || :keyword || '%') "
+            + "or upper(e.lastName) like upper('%' || :keyword || '%')")
+    List<Employee> findByName2(@Param("keyword") String name);
+
+    // 부서 이름으로 검색
+    @Query("select e from Employee e where e.department.departmentName = :dname")
+    List<Employee> findByDeptName(@Param("dname") String deptName);
+
+    // 특정 도시(예: Seattle)에 근무하는 직원들 검색
+    @Query("select e from Employee e "
+            + "where e.department.location.city = :city")
+    List<Employee> findByCity(@Param("city") String cityName);
+
+    // 특정 국가(예: Canada)에 근무하는 직원들 검색
+    @Query("select e from Employee e "
+            +"where e.department.location.country.countryName = :country")
+    List<Employee> findByCountry(@Param("country") String countryName);
 }
--- springboot3/src/test/java/com/itwill/springboot3/repository/JpaQueryMethodTest.java
+++ springboot3/src/test/java/com/itwill/springboot3/repository/JpaQueryMethodTest.java
@@ -18,13 +18,28 @@ public class JpaQueryMethodTest {
     
     @Test
     public void test() {
+      log.info("test()");
        List<Employee> list;
-    //    list =  empRepo.findByDepartmentId(30);
-    // list = empRepo.findByFirstName("Steven");
-    // list = empRepo.findByFirstNameContaining("te");
-    // list = empRepo.findByFirstNameLike("%te%");
-    // list = empRepo.findByFirstNameContainingIgnoreCase("Te");
-    list = empRepo.findByFirstNameContainingIgnoreCaseOrderByFirstNameDesc("Te");
+   // list =  empRepo.findByDepartmentId(30);
+   // list = empRepo.findByFirstName("Steven");
+   // list = empRepo.findByFirstNameContaining("te");
+   // list = empRepo.findByFirstNameLike("%te%");
+   // list = empRepo.findByFirstNameContainingIgnoreCase("Te");
+   // list = empRepo.findByFirstNameContainingIgnoreCaseOrderByFirstNameDesc("Te");
+   // list = empRepo.findBySalaryGreaterThan(10_000);
+   // list = empRepo.findBySalaryLessThan(10_000);
+   // list = empRepo.findBySalaryBetween(10000, 12000);
+   // list = empRepo.findByHireDateBefore(LocalDate.of(2003,1,1));
+   // list = empRepo.findByHireDateAfter(LocalDate.parse("2007-12-31"));
+   // list = empRepo.findByHireDateBetween(LocalDate.parse("2006-12-31"),LocalDate.parse("2007-12-31"));
+   // list = empRepo.findByDepartmentDepartmentName("IT");
+   // list = empRepo.findByDepartmentLocationCity("Seattle");
+   // list = empRepo.findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase("te", "te");
+   // list = empRepo.findByName("TE", "te");
+   // list = empRepo.findByName2("te");
+   // list = empRepo.findByDeptName("IT");
+   // list = empRepo.findByCity("Seattle");
+   list = empRepo.findByCountry("Canada");
 
        list.forEach(System.out::println);
     }
