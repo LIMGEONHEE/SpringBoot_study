43dd42ecf879cb186f43dbd56a51242299a59f1c
LIMGEONHEE <dlarjsgml321@naver.com>
2024-07-25 15:17:46 +0900

    7/25

--- springboot3/src/main/java/com/itwill/springboot3/service/EmployeeSerivce.java
+++ springboot3/src/main/java/com/itwill/springboot3/service/EmployeeSerivce.java
@@ -2,9 +2,13 @@ package com.itwill.springboot3.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import com.itwill.springboot3.domain.Employee;
+import com.itwill.springboot3.domain.Employee; // 페이징 처리가 되어 있는 객체
 import com.itwill.springboot3.dto.EmployeeListItemDto;
 import com.itwill.springboot3.repository.EmployeeRepository;
 
@@ -18,18 +22,33 @@ public class EmployeeSerivce {
 
 	private final EmployeeRepository empRepo;
 	
-	public List<EmployeeListItemDto> read() {
-		log.info("read()");
+	public Page<EmployeeListItemDto> read(int pageNo, Sort sort) { // org.springframework.data.domain.Sort으로 선택. (페이지 넘버, 정렬 기준)
+		log.info("read(pageNo={}, sort={})", pageNo, sort);
 		
-		List<Employee> list = empRepo.findAll();
-		log.info("emp list size = {}", list.size());
+		// Pageable 객체 생성: PageRequest.of(페이지번호, 한 페이지의 아이템 개수, 정렬 기준)
+		Pageable pageable = PageRequest.of(pageNo, 10, sort); 
 
-		return list.stream() // stream은 list 원소의 통로
-				.map(EmployeeListItemDto::fromEntity) // (x) -> EmployeeListItemDto.fromEntity(x), 맵핑
-				.toList(); // 가져온걸 리스트에 담음.
+		// findAll(Pageable pageable) 메서드를 호출.
+        Page<Employee> page = empRepo.findAll(pageable);
+        log.info("hasPrevious = {}", page.hasPrevious()); // 이전 페이지가 있는 지 여부
+        log.info("hasNext = {}", page.hasNext()); // 다음 페이지가 있는 지 여부
+        log.info("getTotalPages = {}", page.getTotalPages()); // 전체 페이지 개수
+        
+        // Page<Employee> 타입을 Page<EmployeeListItemDto> 타입으로 변환해서 리턴.
+        // (x) -> EmployeeListItemDto.fromEntity(x)
+        return page.map(EmployeeListItemDto::fromEntity);
+
+
+		// 페이징처리 하기 이전 코드
+		// List<Employee> list = empRepo.findAll();
+		// log.info("emp list size = {}", list.size());
+
+		// return list.stream() // stream은 list 원소의 통로
+		// 		.map(EmployeeListItemDto::fromEntity) // (x) -> EmployeeListItemDto.fromEntity(x), 맵핑
+		// 		.toList(); // 가져온걸 리스트에 담음.
 	}
 	
-	public Employee employeeDetails(Integer id) {
+	public Employee read(Integer id) {
 		log.info("employeeDetails(id={})", id);
 		
 		return empRepo.findById(id).orElseThrow();
--- springboot3/src/main/java/com/itwill/springboot3/web/EmployeeController.java
+++ springboot3/src/main/java/com/itwill/springboot3/web/EmployeeController.java
@@ -2,11 +2,14 @@ package com.itwill.springboot3.web;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.itwill.springboot3.domain.Employee;
 import com.itwill.springboot3.dto.EmployeeListItemDto;
@@ -25,24 +28,24 @@ public class EmployeeController {
 	private final EmployeeSerivce empSvc;
 	
 	@GetMapping("/list")
-	public void list(Model model) {
-		log.info("list()");
+	public void list(@RequestParam(name = "p", defaultValue = "0") int pageNo, Model model) { // requestparam에 "p" 가 있으면 넣고, 없으면 "0"으로 넣어라
+		log.info("list(pageNo={})", pageNo);
 		
 		// 서비스(비즈니스) 계층의 메서드를 호출해서 뷰에 전달할 직원 목록을 가져옴.
-		List<EmployeeListItemDto> list = empSvc.read();
-		model.addAttribute("employees", list); // 뷰에서는 employees로 부르면 된다.
+		Page<EmployeeListItemDto> list = empSvc.read(pageNo, Sort.by("id"));
+        model.addAttribute("page", list); // 뷰에서는 employees로 부르면 된다.
 	
 	}
 	
 	@GetMapping("/details/{id}")
-	public String empDetails(@PathVariable(name = "id") Integer id, Model model) {
-		log.info("empDetails()");
-		
-		Employee emp = empSvc.employeeDetails(id);
-		model.addAttribute("employee", emp);
-		
-		return "employee/details";
-		
-	}
+	public String details(@PathVariable("id") Integer id, Model model) { // void로 하면 {id}부분이 {id}로 주소 읽힘. 그래서 String 으로 해야함.
+		log.info("details(id={})", id);
+        
+        // 서비스(비즈니스) 계층의 메서드를 호출해서 직원 상세 정보를 가져옴.
+        Employee emp = empSvc.read(id);
+        model.addAttribute("employee", emp);
+        
+        return "employee/details";
+    }
 
 }
