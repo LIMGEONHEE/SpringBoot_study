0bd25b301695c1287c5ee9afb331ffa8f037b103
LIMGEONHEE <dlarjsgml321@naver.com>
2024-07-29 12:18:58 +0900

    7/29

--- springboot5/src/main/java/com/itwill/springboot5/domain/BaseTimeEntity.java
+++ springboot5/src/main/java/com/itwill/springboot5/domain/BaseTimeEntity.java
@@ -17,7 +17,7 @@ import lombok.ToString;
 @Getter // getter 메서드
 @ToString // toString()
 @EqualsAndHashCode // equals(), hashCode(), canEqual()
-@ MappedSuperclass // 다른 엔터티 클래스의 상위 클래스로 사용됨.
+@MappedSuperclass // 다른 엔터티 클래스의 상위 클래스로 사용됨.
 @EntityListeners(AuditingEntityListener.class)
 //-> 엔터티 (최초) 생성시간, (최종) 수정시간 등을 자동으로 DB에 저장하기 위해서. 예약처럼 날짜를 선택하는 경우에는 사용X.
 public class BaseTimeEntity {  // 여러 클래스에 공통되어 사용되기 때문에 따로 만들어서 상속.
